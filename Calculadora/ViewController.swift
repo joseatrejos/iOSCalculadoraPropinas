15164811ee394e0144ea01344a193595ca4525cc
Alumno <ulsa@iMacD115.local>
2019-08-09 13:27:47 -0700

    Primer Commit

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sldPorcentajePropina: UISlider!
+    @IBOutlet weak var lblCantidadPropina: UILabel!
+    @IBOutlet weak var lblPorcentajePropina: UILabel!
+    @IBOutlet weak var txtTotalSinPropina: UITextField!
+    @IBOutlet weak var lblTotalConPropina: UILabel!
+    
+    
+    @IBAction func doChangeValores(_ sender: Any) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
