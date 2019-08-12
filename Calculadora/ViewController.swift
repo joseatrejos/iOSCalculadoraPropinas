636d09853417f7a8fcdb58d441c2404c34e70692
Alumno <ulsa@iMacD115.local>
2019-08-12 14:47:10 -0700

    Formato de los valores

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -19,20 +19,17 @@ class ViewController: UIViewController {
     @IBAction func doChangeValores(_ sender: Any) {
         
         var porcentajePropina = 0.0
-        porcentajePropina = Double(sldPorcentajePropina.value)
+        porcentajePropina = (Double(sldPorcentajePropina.value))
         porcentajePropina.round()
-        lblPorcentajePropina.text = "\(sldPorcentajePropina.value)%"
-        porcentajePropina = porcentajePropina / 100
+        lblPorcentajePropina.text = "\(String(format: "%.0f", porcentajePropina))%"
+        
         
         // "!" indica que el desarrollador se encargara de que la variable no este vacia
-        let cantidadPropina = Double(txtTotalSinPropina.text!)! * porcentajePropina
+        let cantidadPropina = Double(txtTotalSinPropina.text!)! * (porcentajePropina / 100)
         let totalConPropina = cantidadPropina + Double(txtTotalSinPropina.text!)!
         
-        let cantidadPropinaFormatted = String(format: "%.2f", cantidadPropina)
-        let totalConPropinaFormatted = String(format: "%.2f", totalConPropina)
-        
-        lblCantidadPropina.text! = "$\(cantidadPropinaFormatted)"
-        lblTotalConPropina.text! = "$\(totalConPropinaFormatted)"
+        lblCantidadPropina.text! = "$\(String(format: "%.2f", cantidadPropina))"
+        lblTotalConPropina.text! = "$\(String(format: "%.2f", totalConPropina))"
     }
     
     override func viewDidLoad() {
